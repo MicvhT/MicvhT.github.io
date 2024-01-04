a4e82f136542d8a9d1c8ad237df45344d399b229
MicvhT <148805181+MicvhT@users.noreply.github.com>
2024-01-03 23:31:46 -0600

    Add files via upload

--- indexScripts.js
+++ indexScripts.js
@@ -5,3 +5,7 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+// let gatherText;
+// gatherText = document.getElementById("testingThis").innerText;
+// console.log(gatherText);
